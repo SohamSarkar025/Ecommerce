4801034379653ec1a8776c4fbbab9d53939a1b88
Soham Sarkar <srutidhara153@gmail.com>
2025-07-23 19:55:59 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -13,23 +13,26 @@ const adminRoutes = require("./routes/adminRoutes");
 const productAdminRoutes = require("./routes/productAdminRoutes");
 const adminOrderRoutes = require("./routes/adminOrderRoutes");
 
+dotenv.config();
+connectDB();
+
 const app = express();
 app.use(express.json());
-app.use(cors());
 
-dotenv.config();
-
-// console.log(process.env.PORT);
+app.use(
+  cors({
+    origin: "https://soham-ecom.onrender.com/",
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 
-//MongoDB Connnection
-connectDB();
-
 app.get("/", (req, res) => {
-  res.send("Welcome to api");
+  res.send("API is running...");
 });
-//API Routes
+
+// API Routes
 app.use("/api/user", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
@@ -37,23 +40,9 @@ app.use("/api/checkout", checkoutRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/subscribe", subscribeRoutes);
-
-//Admin
 app.use("/api/admin/users", adminRoutes);
 app.use("/api/admin/products", productAdminRoutes);
 app.use("/api/admin/orders", adminOrderRoutes);
-
-const path = require("path");
-
-// Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-}
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
